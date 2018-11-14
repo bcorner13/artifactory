6934ddb1412aa90b31893cda5ee31e3641c904aa
bcorner <bradley.corner@greensky.com>
2018-11-14 16:59:03 -0500

    Created a rpm installation method

--- recipes/_rpm_install.rb
+++ recipes/_rpm_install.rb
@@ -12,71 +12,36 @@ if node['artifactory']['install_java']
   node.normal['java']['jdk_version'] = node['artifactory']['java']['jdk_version']
   include_recipe 'java'
 end
+# default['artifactory'] = {
+#   'pro' => { 'rpm_url': 'https://bintray.com/jfrog/artifactory-pro-rpms/rpm ',
+#              'install_name': 'jfrog-artifactory-pro' },
+#   'ce' => { 'rpm_url': 'https://bintray.com/jfrog/artifactory-rpms/rpm',
+#             'install_name': 'jfrog-artifactory-cpp-ce' },
+#   'oss' => { 'rpm_url': 'https://bintray.com/jfrog/artifactory-rpms/rpm',
+#              'install_name': 'jfrog-artifactory-oss' },
+# }
 
-include_recipe 'runit'
-package 'unzip'
-# ark requires rsync package
-package 'rsync'
+log node['artifactory']['artifactory_type']
 
-user node['artifactory']['user'] do
-  home node['artifactory']['home']
+log node['artifactory']['rpm'][node['artifactory']['artifactory_type']]['rpm_url']
+log node['artifactory']['rpm'][node['artifactory']['artifactory_type']]['install_name']
+yum_repository 'artifactory' do
+  description 'Jfrog Artifactory Stable repo'
+  baseurl node['artifactory']['rpm'][node['artifactory']['artifactory_type']]['rpm_url']
+  gpgcheck false
+  repo_gpgcheck false
+  action :create
 end
-
-group node['artifactory']['group'] do
-  members [node['artifactory']['user']]
-end
-
-directory node['artifactory']['home'] do
-  owner node['artifactory']['user']
-  group node['artifactory']['group']
-  mode 0o0755
-  recursive true
-end
-
-directory node['artifactory']['catalina_base'] do
-  owner node['artifactory']['user']
-  group node['artifactory']['group']
-  mode 0o0755
-  recursive true
-end
-
-%w(work temp).each do |tomcat_dir|
-  directory ::File.join(node['artifactory']['catalina_base'], tomcat_dir) do
-    owner node['artifactory']['user']
-    group node['artifactory']['group']
-    mode 0o0755
-  end
-end
-
-directory node['artifactory']['log_dir'] do
-  owner node['artifactory']['user']
-  group node['artifactory']['group']
-  mode 0o0755
-end
-ark 'artifactory' do
-  url node['artifactory']['zip_url']
-  checksum node['artifactory']['zip_checksum']
-  owner node['artifactory']['user']
-  group node['artifactory']['group']
+package node['artifactory']['rpm'][node['artifactory']['artifactory_type']]['install_name'] do
+  version node['artifactory']['artifactory_version']
   action :install
 end
-
-link ::File.join(node['artifactory']['home'], 'webapps') do
-  to '/usr/local/artifactory/webapps'
+service 'artifactory' do
+  supports status: true
+  action [:enable, :start]
 end
-
-link ::File.join(node['artifactory']['catalina_base'], 'logs') do
-  to node['artifactory']['log_dir']
-end
-
-link ::File.join(node['artifactory']['catalina_base'], 'conf') do
-  to '/usr/local/artifactory/tomcat/conf'
-end
-
-template '/usr/local/artifactory/tomcat/conf/server.xml' do
-  mode 0o0644
-  notifies :restart, 'runit_service[artifactory]'
-end
-
-
-runit_service 'artifactory'
+# TODO: copy/import configuration and licenses
+# template '/usr/local/artifactory/tomcat/conf/server.xml' do
+#   mode 0o0644
+#   notifies :restart, 'service[artifactory]'
+# end
