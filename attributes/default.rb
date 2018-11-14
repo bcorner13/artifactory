7053b2f6849fcb580d883fa89cc58a27d16b2aee
bcorner <bradley.corner@greensky.com>
2018-11-14 16:58:38 -0500

    Added rpm/apt specific repository attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,23 +2,25 @@
 # source code URL http://dl.bintray.com/content/jfrog/artifactory/
 
 module Artifactory
-  comment 'This defines the version of artifactory to install'
+  # comment 'This defines the version of artifactory to install'
   class << self
     attr_accessor :version
   end
 end
 
-Artifactory.version = '5.5.1'
-default['artifactory']['artifactory_version '] = Artifactory.version
+Artifactory.version = '6.4.1'
+default['artifactory']['artifactory_version'] = Artifactory.version
+default['artifactory']['artifactory_type'] = 'pro'
 default['artifactory']['install_type'] = 'rpm'
+
 default['artifactory']['zip_url'] = "http://dl.bintray.com/content/jfrog/artifactory/jfrog-artifactory-oss-#{Artifactory.version}.zip"
 default['artifactory']['zip_checksum'] = '993d9abd188a6b1eaf4c762f201f0ba70f545ca68ad099f9a9a48bec3470d68d'
 default['artifactory']['home'] = '/var/lib/artifactory'
 default['artifactory']['log_dir'] = '/var/log/artifactory'
 default['artifactory']['catalina_base'] = ::File.join(node['artifactory']['home'], 'tomcat')
-default['artifactory']['java']['xmx'] = '1g'
+default['artifactory']['java']['xmx'] = '2g'
 default['artifactory']['java']['xms'] = '512m'
-default['artifactory']['java']['extra_opts'] = '-XX:+UseG1GC'
+default['artifactory']['java']['extra_opts'] = '-Xss256k -XX:+UseG1GC'
 default['artifactory']['java']['jdk_version'] = 8
 
 default['artifactory']['user'] = 'artifactory'
@@ -35,5 +37,20 @@ default['artifactory']['ajp']['enableLookups'] = false
 default['artifactory']['ajp']['backlog'] = 100
 
 # RPM based installations
-default['artifactory']['repo_url'] = nil
-default['artifactory']['version '] = '5.5.1'
+default['artifactory']['rpm'] = {
+  'pro' => { 'rpm_url' => 'https://jfrog.bintray.com/artifactory-pro-rpms',
+             'install_name' => 'jfrog-artifactory-pro' },
+  'ce' => { 'rpm_url' => 'https://jfrog.bintray.com/artifactory-pro-rpms',
+            'install_name' => 'jfrog-artifactory-cpp-ce' },
+  'oss' => { 'rpm_url' => 'https://jfrog.bintray.com/artifactory-pro-rpms',
+             'install_name' => 'jfrog-artifactory-oss' },
+}
+# APT based installations
+default['artifactory']['apt'] = {
+  'pro' => { 'url' => 'https://jfrog.bintray.com/artifactory-pro-debs/',
+             'install_name' => 'jfrog-artifactory-pro' },
+  'ce' => { 'url' => 'https://jfrog.bintray.com/artifactory-pro-debs/',
+            'install_name' => 'jfrog-artifactory-cpp-ce' },
+  'oss' => { 'url' => 'https://jfrog.bintray.com/artifactory-pro-debs/',
+             'install_name' => 'jfrog-artifactory-oss' },
+}
